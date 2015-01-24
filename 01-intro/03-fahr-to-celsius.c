45209eccbd2fb106e6dd2289159636a54650cd60
finico <finico.a@gmail.com>
2015-01-24 19:14:30 +0300

    1.3 fahr to celsius and added file with tasks

---
+++ 01-intro/03-fahr-to-celsius.c
@@ -0,0 +1,19 @@
+#import <stdio.h>
+
+int main() {
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	printf("Таблица температур по Фаренгейту и Цельсию от 0 до 300 по Фаренгейту\n");
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
